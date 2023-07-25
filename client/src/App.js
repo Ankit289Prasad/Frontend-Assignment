558d418504d17d8adbb107abbcaec9f4ac379f86
Ankit Prasad <ankit28prasad9@gmail.com>
2023-07-26 00:04:00 +0530

    Progress Initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,85 @@
-function App() {
-	return (
-		<h1>Hello world</h1>
-	);
-}
+import React, { useState } from 'react';
+import './App.css'; // Import the custom CSS file for styling
+
+const App = () => {
+  const [symbol, setSymbol] = useState('');
+  const [date, setDate] = useState('');
+  const [tradeData, setTradeData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch('http://localhost:5000/api/fetchStockData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ symbol, date }),
+      });
+
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        setError(errorResponse.error || 'Something went wrong.');
+        setTradeData(null);
+      } else {
+        const data = await response.json();
+        setTradeData(data);
+        setError(null);
+      }
+    } catch (error) {
+      setError('Something went wrong.');
+      setTradeData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Trade Statistics</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label className="label">Stock Symbol:</label>
+          <input
+            className="input"
+            type="text"
+            value={symbol}
+            onChange={(e) => setSymbol(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label className="label">Date:</label>
+          <input
+            className="input"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </div>
+        <button className="submit-button" type="submit" disabled={loading}>
+          {loading ? 'Fetching...' : 'Fetch Data'}
+        </button>
+      </form>
+      {error && <p className="error">{error}</p>}
+      {tradeData && (
+        <div className="trade-data">
+          <h2>Trade Data for {symbol} on {date}</h2>
+          <p>Open: {tradeData.open}</p>
+          <p>High: {tradeData.high}</p>
+          <p>Low: {tradeData.low}</p>
+          <p>Close: {tradeData.close}</p>
+          <p>Volume: {tradeData.volume}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -27,9 +27,33 @@ app.use(bodyParser.json());
 
 app.enable('trust proxy');
 
-app.post('/api/fetchStockData', (req, res) => {
+app.post('/api/fetchStockData', async (req, res) => {
     // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+    try {
+        // Extract the symbol and date from the request body
+        const { symbol, date } = req.body;
+    
+        // Format the date in 'YYYY-MM-DD' format as required by the Polygon API
+        const formattedDate = new Date(date).toISOString().split('T')[0];
+    
+        // Make a request to the Polygon API
+        const response = await axios.get(
+          `https://api.polygon.io/v1/open-close/${symbol}/${formattedDate}?apiKey=Kws8ISdvd6dirYWjrnc1wxFCUN2Xj2oG`
+        );
+    
+        // Extract the relevant data from the API response
+        const { open, high, low, close, volume } = response.data;
+    
+        // Return the data in JSON format
+        res.json({ open, high, low, close, volume });
+      } catch (error) {
+        // Handle errors and return relevant response codes
+        if (error.response && error.response.status === 404) {
+          res.status(404).json({ error: 'Stock data not found for the given date.' });
+        } else {
+          res.status(500).json({ error: 'Internal server error.' });
+        }
+      }
 });
 
 const port = process.env.PORT || 5000;
